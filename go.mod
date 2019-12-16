b3fbf2ddac1df0d9c21c7f710f2c9044ab1c1b4f
Wade Simmons <wadey@slack-corp.com>
2019-12-16 16:03:48 -0500

    convert to go-diff-mod

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/wadey/go-list-updates
+module github.com/wadey/go-diff-mod
 
-go 1.12
+go 1.13
--- main.go
+++ main.go
@@ -4,18 +4,19 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
-	"os"
-	"os/exec"
 	"regexp"
 	"strings"
 )
 
 var (
-	flagDirect = flag.Bool("direct", false, "Only list updates to direct dependencies.")
+	flagDirect   = flag.Bool("direct", false, "Only list updates to direct dependencies.")
+	flagIndirect = flag.Bool("indirect", false, "Only list updates to indirect dependencies.")
+	flagMarkdown = flag.Bool("markdown", false, "Output in markdown format.")
 
-	pseudoVersion = regexp.MustCompile(`^v[0-9]+\.[0-9]+\.[0-9]+-(?:pre.0.|0.)?[0-9]{14}-([0-9a-f]{12}$)`)
+	postfixVersion = regexp.MustCompile(`/(v[0-9]+)$`)
+	pseudoVersion  = regexp.MustCompile(`^v[0-9]+\.[0-9]+\.[0-9]+-(?:pre.0.|0.)?[0-9]{14}-([0-9a-f]{12}$)`)
 )
 
 type Module struct {
@@ -23,50 +24,154 @@ type Module struct {
 	Update   *Module
 	Path     string
 	Version  string
+
+	httpPath string
 }
 
 func main() {
 	flag.Parse()
 
-	cmd := exec.Command("go", "list", "-u", "-m", "-json", "all")
-	cmd.Stderr = os.Stderr
-	stdout, err := cmd.StdoutPipe()
+	if flag.NArg() != 2 {
+		log.Fatalf(`usage: go-diff-mod <old json> <new json>
+
+Directions:
+
+  1. Run "go list -m -json all | jq -s . >before.json"
+  2. Do your package upgrades / installs
+  3. Run "go list -m -json all | jq -s . >after.json"
+  4. Run "go-diff-mod before.json after.json"
+`)
+	}
+
+	oldRaw, err := ioutil.ReadFile(flag.Arg(0))
 	if err != nil {
-		log.Fatalf("failed to run `go list`: %v", err)
+		log.Fatalf("failed to read %s: %v", flag.Arg(0), err)
+	}
+	newRaw, err := ioutil.ReadFile(flag.Arg(1))
+	if err != nil {
+		log.Fatalf("failed to read %s: %v", flag.Arg(1), err)
 	}
 
-	if err = cmd.Start(); err != nil {
-		log.Fatalf("failed to run `go list`: %v", err)
+	var oldModules, newModules []*Module
+	if err = json.Unmarshal(oldRaw, &oldModules); err != nil {
+		log.Fatalf("failed to parse %s: %v", flag.Arg(0), err)
+	}
+	if err = json.Unmarshal(newRaw, &newModules); err != nil {
+		log.Fatalf("failed to parse %s: %v", flag.Arg(1), err)
 	}
 
-	dec := json.NewDecoder(stdout)
+	oldMap := map[string]*Module{}
+	for _, m := range oldModules {
+		if postfixVersion.MatchString(m.Path) {
+			// TODO proper to always strip?
+			m.Path = m.Path[0:strings.LastIndex(m.Path, "/")]
+		}
+		oldMap[m.Path] = m
+	}
+	newMap := map[string]*Module{}
+	for _, m := range newModules {
+		if postfixVersion.MatchString(m.Path) {
+			// TODO proper to always strip?
+			m.Path = m.Path[0:strings.LastIndex(m.Path, "/")]
+		}
+		newMap[m.Path] = m
+	}
 
-	for {
-		var m Module
-		if err := dec.Decode(&m); err == io.EOF {
-			break
-		} else if err != nil {
-			log.Fatalf("failed to decode json: %v", err)
+	var changed, added, removed []*Module
+
+	for _, oldM := range oldModules {
+		newM := newMap[oldM.Path]
+		if newM != nil {
+			if newM.Version != oldM.Version {
+				oldM.Update = newM
+				changed = append(changed, oldM)
+			}
+		} else {
+			removed = append(removed, oldM)
+		}
+	}
+	for _, newM := range newModules {
+		oldM := oldMap[newM.Path]
+		if oldM == nil {
+			added = append(added, newM)
+		}
+	}
+
+	for _, m := range changed {
+		output("Updated", m)
+	}
+	for _, m := range added {
+		output("Added", m)
+	}
+	for _, m := range removed {
+		output("Removed", m)
+	}
+
+	// if !(*flagDirect && m.Indirect) && !(*flagIndirect && !m.Indirect) && m.Update != nil {
+	// 	m.Version = pseudoVersion.ReplaceAllString(m.Version, "$1")
+	// 	m.Update.Version = pseudoVersion.ReplaceAllString(m.Update.Version, "$1")
+
+	// 	if strings.HasPrefix(m.Path, "golang.org/x/") {
+	// 		m.httpPath = "github.com/golang/" + strings.TrimPrefix(m.Path, "golang.org/x/")
+	// 	} else {
+	// 		m.httpPath = m.Path
+	// 	}
+
+	// 	switch {
+	// 	case strings.HasPrefix(m.httpPath, "github.com/"):
+	// 		if *flagMarkdown {
+	// 			fmt.Printf("- [%s (%s -> %s)](https://%s/compare/%s...%s)\n", m.Path, m.Version, m.Update.Version, m.httpPath, m.Version, m.Update.Version)
+	// 		} else {
+	// 			fmt.Printf("%s\thttps://%s/compare/%s...%s\n", m.Path, m.httpPath, m.Version, m.Update.Version)
+	// 		}
+	// 	default:
+	// 		fmt.Printf("%s\t%s...%s\n", m.Path, m.Version, m.Update.Version)
+	// 	}
+	// }
+}
+
+func output(label string, m *Module) {
+	if !(*flagDirect && m.Indirect) && !(*flagIndirect && !m.Indirect) {
+		m.Version = pseudoVersion.ReplaceAllString(m.Version, "$1")
+
+		if strings.HasPrefix(m.Path, "golang.org/x/") {
+			m.httpPath = "github.com/golang/" + strings.TrimPrefix(m.Path, "golang.org/x/")
+		} else {
+			m.httpPath = m.Path
 		}
 
-		if !(*flagDirect && m.Indirect) && m.Update != nil {
-			m.Version = pseudoVersion.ReplaceAllString(m.Version, "$1")
+		if m.Update != nil {
 			m.Update.Version = pseudoVersion.ReplaceAllString(m.Update.Version, "$1")
 
-			if strings.HasPrefix(m.Path, "golang.org/x/") {
-				m.Path = "github.com/golang/" + strings.TrimPrefix(m.Path, "golang.org/x/")
-			}
-
 			switch {
-			case strings.HasPrefix(m.Path, "github.com/"):
-				fmt.Printf("https://%s/compare/%s...%s\n", m.Path, m.Version, m.Update.Version)
+			case strings.HasPrefix(m.httpPath, "github.com/"):
+				if *flagMarkdown {
+					fmt.Printf("- %s [%s (%s -> %s)](https://%s/compare/%s...%s)\n", label, m.Path, m.Version, m.Update.Version, m.httpPath, m.Version, m.Update.Version)
+				} else {
+					fmt.Printf("%s\t%s\thttps://%s/compare/%s...%s\n", label, m.Path, m.httpPath, m.Version, m.Update.Version)
+				}
 			default:
-				fmt.Printf("%s - %s - %s\n", m.Path, m.Version, m.Update.Version)
+				if *flagMarkdown {
+					fmt.Printf("- %s %s (%s -> %s)\n", label, m.Path, m.Version, m.Update.Version)
+				} else {
+					fmt.Printf("%s\t%s\t%s...%s\n", label, m.Path, m.Version, m.Update.Version)
+				}
+			}
+		} else {
+			switch {
+			case strings.HasPrefix(m.httpPath, "github.com/"):
+				if *flagMarkdown {
+					fmt.Printf("- %s [%s (%s)](https://%s/tree/%s)\n", label, m.Path, m.Version, m.httpPath, m.Version)
+				} else {
+					fmt.Printf("%s\t%s\thttps://%s/tree/%s\n", label, m.Path, m.httpPath, m.Version)
+				}
+			default:
+				if *flagMarkdown {
+					fmt.Printf("- %s %s (%s)\n", label, m.Path, m.Version)
+				} else {
+					fmt.Printf("%s\t%s\t%s\n", label, m.Path, m.Version)
+				}
 			}
 		}
-	}
-
-	if err = cmd.Wait(); err != nil {
-		log.Fatal(err)
 	}
 }
